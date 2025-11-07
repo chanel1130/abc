2ec14ae404a9d397aa5569448e3ffde81ada7044
chanel1130 <131328076+chanel1130@users.noreply.github.com>
2025-11-07 12:14:35 +0800

    cam location

--- gps-camera/public/sketch.js
+++ gps-camera/public/sketch.js
@@ -3,46 +3,49 @@ let myMap;
 let canvas;
 let currentLongitude = 0; // global variables will be updated as we get GPS data
 let currentLatitude = 0; // global variables will be updated as we get GPS data
+let othersPosOnCanvas;
 let mapInit = false; // we only do map stuff once mapInit is true (see in draw)
 let me; // point object showing our own location
 let others = {};
 
-if(location.hostname.toLowerCase().startsWith('browsercircus') || location.hostname.toLowerCase().startsWith('www')){
-  socket = io({path: "/chanel/port-4200/socket.io"});  // yields '/leon/port-4100/socket.io' or '/socket.io'
-}else{
-  socket = io(); 
+if (location.hostname.toLowerCase().startsWith('browsercircus') || location.hostname.toLowerCase().startsWith('www')) {
+  socket = io({ path: "/chanel/port-4200/socket.io" });  // yields '/leon/port-4100/socket.io' or '/socket.io'
+} else {
+  socket = io();
 }
 
-let cameras = [];
+// let cameras = [];
+let camerasLocation = [];
+let camerasCenter = [];
 
-let deleteButton = document.getElementById("deleteLast");
-
-
-let unlockButton = document.getElementById("unlock");
-let lastButtonClick = -5000;
-let timeToMarkCamera = 5000;
-
-unlockButton.addEventListener("click", function(){
-  lastButtonClick = millis();
-})
-
-let lastLat = 0;
-let lastLng = 0;
-deleteButton.addEventListener("click", function(){
-  // lastButtonClick = millis();
+// let deleteButton = document.getElementById("deleteLast");
 
 
-  socket.emit("deletePrevious");
-  deleteButton.style.display = "none";
-  for(let i = cameras.length-1; i >= 0 ; i--){
-    if(cameras[i].lat == lastLat && cameras[i].lng == lastLng){
-      console.log(cameras)
-      cameras.splice(i, 1);
-      console.log(cameras)
-    } 
+// let unlockButton = document.getElementById("unlock");
+// let lastButtonClick = -5000;
+// let timeToMarkCamera = 5000;
 
-  }
-})
+// unlockButton.addEventListener("click", function () {
+//   lastButtonClick = millis();
+// })
+
+// let lastLat = 0;
+// let lastLng = 0;
+// deleteButton.addEventListener("click", function () {
+//   // lastButtonClick = millis();
+
+
+//   socket.emit("deletePrevious");
+//   deleteButton.style.display = "none";
+//   for (let i = cameras.length - 1; i >= 0; i--) {
+//     if (cameras[i].lat == lastLat && cameras[i].lng == lastLng) {
+//       console.log(cameras)
+//       cameras.splice(i, 1);
+//       console.log(cameras)
+//     }
+
+//   }
+// })
 
 
 // options for map
@@ -61,7 +64,16 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5-canvas-container");
   me = new MyPoint();
-  // others = new MyPoint();
+
+
+  //load the json file
+  loadJSON('camera-location.json', function (data) {
+    camerasLocation = data;
+    // console.log("Loaded cameras:", camerasLocation);
+
+    //for drawing lines
+    camerasCenter = data;
+  });
 
 }
 
@@ -69,16 +81,17 @@ function draw() {
   clear();
 
 
-  if(millis()-lastButtonClick<timeToMarkCamera){
-    let timeLeft = floor(((5000-(millis()-lastButtonClick))/1000));
-    // text(timeLeft, 50, 300)
-    unlockButton.innerHTML = "mark camera (" + timeLeft + ")"
-    unlockButton.style.color = "red";
-  }else{
-     unlockButton.innerHTML = "UNLOCK";
-     unlockButton.style.color = "green";
-  }
-  // Initialize full screen map
+  // if (millis() - lastButtonClick < timeToMarkCamera) {
+  //   let timeLeft = floor(((5000 - (millis() - lastButtonClick)) / 1000));
+  //   // text(timeLeft, 50, 300)
+  //   unlockButton.innerHTML = "mark camera (" + timeLeft + ")"
+  //   unlockButton.style.color = "red";
+  // } else {
+  //   unlockButton.innerHTML = "UNLOCK";
+  //   unlockButton.style.color = "green";
+  // }
+
+  // // Initialize full screen map
   if (!mapInit && GPS_GRANTED && currentLongitude != 0) {
     console.log("starting map");
     mappa_options.lat = currentLatitude;
@@ -88,28 +101,75 @@ function draw() {
     myMap = mappa.tileMap(mappa_options);
     myMap.overlay(canvas);
     myMap.onChange(updateMapContent);
-    unlockButton.style.display = "block";
-    deleteButton.style.display = "block";
+    // unlockButton.style.display = "block";
+    // deleteButton.style.display = "block";
     mapInit = true
   }
 
   if (mapInit) {
 
-    for (let id in others) {
-      others[id].update();
-      others[id].display();
+    //camera centers (for drawing lines)
+    for (let i = 0; i < camerasCenter.length; i++) {
+      let c = camerasCenter[i];
+      // cameras.push({ lat: c.lat, lng: c.lng });
+      let camPos = myMap.latLngToPixel(c.lat, c.lng);
+      fill(255);
+      noStroke();
+      circle(camPos.x, camPos.y, 4);
+
     }
 
-
-    for (let i = 0; i < cameras.length; i++) {
-      let c = cameras[i];
+    //lines
+    for (let i = 0; i < camerasCenter.length; i++) {
+      let c = camerasCenter[i];
       let d = getDistanceFromLatLonInM(currentLatitude, currentLongitude, c.lat, c.lng);
       if (d < 100) {
         stroke(0, 0, 255);
         strokeWeight(1);
         line(me.x, me.y, c.x, c.y);
       }
+
     }
+
+    //camera logos
+    for (let i = 0; i < camerasLocation.length; i++) {
+      let c = camerasLocation[i];
+      // cameras.push({ lat: c.lat, lng: c.lng });
+      let camPos = myMap.latLngToPixel(c.lat, c.lng);
+      // fill(255);
+      // noStroke();
+      // circle(camPos.x, camPos.y, 10);
+      fill(150, 7, 7);
+      noStroke();
+      circle(camPos.x, camPos.y, 20);
+      triangle(camPos.x - 7.45, camPos.y + 6.67, camPos.x + 7.45, camPos.y + 6.67, camPos.x, camPos.y + 17);
+      fill(220);
+      circle(camPos.x, camPos.y, 16);
+      fill(50);
+      rectMode(CENTER);
+      rect(camPos.x, camPos.y, 12, 9, 2);
+      rect(camPos.x + 2, camPos.y - 5, 4, 2, 0.2);
+      stroke(200);
+      strokeWeight(0.8);
+      circle(camPos.x, camPos.y, 4.5);
+
+    }
+
+
+
+    for (let id in others) {
+      others[id].update();
+      others[id].display();
+    }
+
+    // for (let i = 0; i < cameras.length; i++) {
+    //   let c = cameras[i];
+    //   //from location to screen pixel
+    //   let camPos = myMap.latLngToPixel(c.lat, c.lng);
+    //   fill(255);
+    //   noStroke();
+    //   circle(camPos.x, camPos.y, 10);
+    // }
 
 
     // only update and draw our point if we actually have data
@@ -118,16 +178,6 @@ function draw() {
     // console.log(me)
 
   }
-  for (let i = 0; i < cameras.length; i++) {
-    let c = cameras[i];
-    //from location to screen pixel
-    let camPos = myMap.latLngToPixel(c.lat, c.lng);
-    fill(255);
-    noStroke();
-    circle(camPos.x, camPos.y, 10);
-  }
-
-
 
 }
 
@@ -144,27 +194,27 @@ function touchStarted() {
 
 
 
-    if(millis()-lastButtonClick<timeToMarkCamera){
-      //from screen pixel to location
-      let pos = myMap.pixelToLatLng(touches[0].x, touches[0].y);
-      console.log("TOUCHED", pos);
-      cameras.push({
-        lat: pos.lat,
-        lng: pos.lng
-      })
-      lastLat = pos.lat;
-      lastLng = pos.lng;
-      socket.emit("camera-from-client", {
-        lat: pos.lat,
-        lng: pos.lng
-      })
-      console.log(cameras)
-      lastButtonClick = 0;
-      deleteButton.style.display = "block";
+    // if (millis() - lastButtonClick < timeToMarkCamera) {
+    //   //from screen pixel to location
+    //   let pos = myMap.pixelToLatLng(touches[0].x, touches[0].y);
+    //   console.log("TOUCHED", pos);
+    //   cameras.push({
+    //     lat: pos.lat,
+    //     lng: pos.lng
+    //   })
+    //   lastLat = pos.lat;
+    //   lastLng = pos.lng;
+    //   socket.emit("camera-from-client", {
+    //     lat: pos.lat,
+    //     lng: pos.lng
+    //   })
+    //   console.log(cameras)
+    //   lastButtonClick = 0;
+    //   deleteButton.style.display = "block";
 
-    }
-    
-    
+    // }
+
+
 
 
   } else {
@@ -195,11 +245,11 @@ function handleNewPosition(pos) {
     updateMapContent();
   }
   let locForSer = {
-    lat: currentLatitude, 
+    lat: currentLatitude,
     lng: currentLongitude
   }
   socket.emit("location-from-client", { locForSer });
- 
+
 }
 // socket.on("location-from-server", function (data) {
 //   console.log("other location", data);
@@ -213,13 +263,14 @@ socket.on("location-from-server", function (data) {
   // others.goalX = othersPosOnCanvas.x;
   // others.goalY = othersPosOnCanvas.y;
 
+  //if first sees this id - gives it a MyPoint()
   if (!others[data.id]) {
     others[data.id] = new MyPoint();
   }
 
-  let pos = myMap.latLngToPixel(data.lat, data.lng);
-  others[data.id].goalX = pos.x;
-  others[data.id].goalY = pos.y;
+  othersPosOnCanvas = myMap.latLngToPixel(data.lat, data.lng);
+  others[data.id].goalX = othersPosOnCanvas.x;
+  others[data.id].goalY = othersPosOnCanvas.y;
 })
 
 
@@ -229,12 +280,9 @@ function updateMapContent() {
   me.goalY = myPosOnCanvas.y;
 
 
-
-
-
-  //update the camera locations
-  for (let i = 0; i < cameras.length; i++) {
-    let c = cameras[i];
+  //update the camera center locations
+  for (let i = 0; i < camerasCenter.length; i++) {
+    let c = camerasCenter[i];
     let camPixel = myMap.latLngToPixel(c.lat, c.lng);
     c.x = camPixel.x;
     c.y = camPixel.y;
@@ -265,11 +313,11 @@ class MyPoint {
     noStroke();
     let dia = this.size + sin(frameCount * 0.1)
     circle(0, 0, dia);
-
     pop();
   }
 }
 
+//found online
 function getDistanceFromLatLonInM(lat1, lon1, lat2, lon2) {
   const R = 6371000; // radius of the earch
   const dLat = radians(lat2 - lat1);
