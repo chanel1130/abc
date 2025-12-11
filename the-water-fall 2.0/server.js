68df04d1007913b256f432249cc2f3df2a633d54
chanel1130 <131328076+chanel1130@users.noreply.github.com>
2025-12-11 15:02:46 +0800

    1

--- the-water-fall 2.0/server.js
+++ the-water-fall 2.0/server.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 
 const app = express(); // the server "app", the server behaviour
 
-const portHTTPS = 4205; // port for https
+const portHTTPS = 4201; // port for https
 
 // returning to the client anything that is
 // inside the public folder
